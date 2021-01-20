4cc1630c14c0445a722cb884eae30ac72b38b602
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2021-01-20 03:03:09 +0300

    feat: add dto entity.

---
+++ Services/Identity/Identity/Common/Interfaces/IUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace Identity.Common.Interfaces
+{
+    /// <summary>
+    /// Interface for service to manage user accounts.
+    /// </summary>
+    public interface IUserService
+    {
+    }
+}
---
+++ Services/Identity/Identity/DTO/UserDTO.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.DTO
+{
+    public class UserDTO
+    {
+        /// <summary>
+        /// Identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// User email.
+        /// </summary>
+        [Required]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// User username.
+        /// </summary>
+        [Required]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// User password.
+        /// </summary>
+        [Required]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// User role.
+        /// </summary>
+        public int Role { get; set; }
+    }
+}
