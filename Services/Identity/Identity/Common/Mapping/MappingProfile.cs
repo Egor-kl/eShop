8e1cb5372ff873d1068c697a6fbf2bb1aef4e106
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2021-01-26 10:02:18 +0300

    feat: add mapping

---
+++ Services/Identity/Identity/Common/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Identity.DTO;
+using Identity.Models;
+
+namespace Identity.Common.Mapping
+{
+    /// <summary>
+    /// Define Automapper profile for Identity entities.
+    /// </summary>
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+        }
+    }
+}
