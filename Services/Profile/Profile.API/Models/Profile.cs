dfd20255595b2f2da76f0bd38d09d443964d0e70
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2021-02-01 03:26:45 +0300

    feat: add profile entity

---
+++ Services/Profile/Profile.API/Models/Profile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Profile.API.Models
+{
+    /// <summary>
+    /// Profile entity.
+    /// </summary>
+    public class Profile
+    {
+        /// <summary>
+        /// First name user.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last name user.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Date of birthday user.
+        /// </summary>
+        public DateTime BirthDate { get; set; }
+
+        /// <summary>
+        /// User avatar.
+        /// </summary>
+        public byte[] Avatars { get; set; }
+
+        /// <summary>
+        /// User discount.
+        /// </summary>
+        public double Discount { get; set; }
+        
+        /// <summary>
+        /// User purchases.
+        /// </summary>
+        public List<string> Purchases { get; set; }
+    }
+}
