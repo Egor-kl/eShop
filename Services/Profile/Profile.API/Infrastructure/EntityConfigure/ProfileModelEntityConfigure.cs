f1c5af36930416dfb0dff8da9f9bf05332d4e25e
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2021-02-05 02:48:16 +0300

    feat: add migrations

---
+++ Services/Profile/Profile.API/Infrastructure/EntityConfigure/ProfileModelEntityConfigure.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Profile.API.Infrastructure.EntityConfigure
+{
+    public class ProfileModelEntityConfigure : IEntityTypeConfiguration<Models.Profile>
+    {
+        /// <summary>
+        /// Configure user model entity.
+        /// </summary>
+        /// <param name="builder">User model builder.</param>
+        public void Configure(EntityTypeBuilder<Models.Profile> builder)
+        {
+            builder.HasKey(user => user.Id);
+
+            builder.Property(user => user.Avatars);
+
+            builder.Property(user => user.FirstName)
+                .HasMaxLength(255);
+
+            builder.Property(user => user.LastName)
+                .HasMaxLength(255);
+
+            builder.Property(user => user.Discount);
+
+            builder.Property(user => user.Purchases);
+
+            builder.Property(user => user.BirthDate);
+
+            builder.Property(user => user.Phone)
+                .HasMaxLength(20);
+        }
+    }
+}
