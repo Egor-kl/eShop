8f3fe99e67cc3877c625af6995bbd28c2a0deb97
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2021-02-19 23:21:08 +0300

    feat: add models

---
+++ Services/Catalog/Catalog.API/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace Catalog.API.Models
+{
+    public class Category
+    {
+        /// <summary>
+        /// Category identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Category name
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
