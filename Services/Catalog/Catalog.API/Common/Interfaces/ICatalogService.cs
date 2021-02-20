1d992ce564fa4692bf5c1cc96a732102eedd9bd6
Egor Kliutsuk <57603709+Egor-kl@users.noreply.github.com>
2021-02-20 14:22:01 +0300

    feat: add catalog interface

---
+++ Services/Catalog/Catalog.API/Common/Interfaces/ICatalogService.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.API.DTO;
+
+namespace Catalog.API.Common.Interfaces
+{
+    public interface ICatalogService
+    {
+        /// <summary>
+        /// Add new product category.
+        /// </summary>
+        /// <returns></returns>
+        public Task<(int id, bool success)> AddNewCategory();
+        
+        /// <summary>
+        /// Add new item.
+        /// </summary>
+        /// <returns></returns>
+        public Task<(int id, bool success)> AddNewItem();
+
+        /// <summary>
+        /// Update category.
+        /// </summary>
+        /// <returns></returns>
+        public Task<bool> UpdateCategory(CategoryDTO categoryDTO);
+        
+        /// <summary>
+        /// Update item
+        /// </summary>
+        /// <returns></returns>
+        public Task<bool> UpdateItem(ItemDTO itemDTO);
+        
+        /// <summary>
+        /// Get all categories.
+        /// </summary>
+        /// <returns></returns>
+        public Task<List<CategoryDTO>> GetAllCategories();
+        
+        /// <summary>
+        /// Get all items.
+        /// </summary>
+        /// <returns></returns>
+        public Task<List<ItemDTO>> GetAllItems();
+
+        /// <summary>
+        /// Get category by name.
+        /// </summary>
+        /// <returns></returns>
+        public Task<CategoryDTO> GetCategoryByName(string name);
+        
+        /// <summary>
+        /// Get Item by name.
+        /// </summary>
+        /// <returns></returns>
+        public Task<ItemDTO> GetItemByName(string name);
+        
+        /// <summary>
+        /// Get category by id.
+        /// </summary>
+        /// <returns></returns>
+        public Task<CategoryDTO> GetCategoryById(int id);
+        
+        /// <summary>
+        /// Get Item by id.
+        /// </summary>
+        /// <returns></returns>
+        public Task<ItemDTO> GetItemById(int id);
+
+        /// <summary>
+        /// Delete item by id.
+        /// </summary>
+        /// <returns></returns>
+        public Task<bool> DeleteItemById(int id);
+        
+        /// <summary>
+        /// Delete item by name.
+        /// </summary>
+        /// <returns></returns>
+        public Task<bool> DeleteItemByName(string name);
+        
+        /// <summary>
+        /// Delete category by id.
+        /// </summary>
+        /// <returns></returns>
+        public Task<bool> DeleteCategoryById(int id);
+        
+        /// <summary>
+        /// Delete category by name.
+        /// </summary>
+        /// <returns></returns>
+        public Task<bool> DeleteCategoryByName(string name);
+    }
+}
---
+++ Services/Catalog/Catalog.API/Services/CatalogService.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.API.Common.Interfaces;
+using Catalog.API.DTO;
+
+namespace Catalog.API.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        public async Task<(int id, bool success)> AddNewCategory()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<(int id, bool success)> AddNewItem()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> UpdateCategory(CategoryDTO categoryDTO)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> UpdateItem(ItemDTO itemDTO)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<List<CategoryDTO>> GetAllCategories()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<List<ItemDTO>> GetAllItems()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<CategoryDTO> GetCategoryByName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<ItemDTO> GetItemByName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<CategoryDTO> GetCategoryById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<ItemDTO> GetItemById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> DeleteItemById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> DeleteItemByName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> DeleteCategoryById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> DeleteCategoryByName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
