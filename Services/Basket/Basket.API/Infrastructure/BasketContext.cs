a860e57cba41f27a1ea35d7c55ba1ad4ec916f22
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2021-03-15 00:15:10 +0300

    feat: add context

---
+++ Services/Basket/Basket.API/Infrastructure/BasketContext.cs
@@ -0,0 +1,15 @@
+﻿using Basket.API.Common.Interfaces;
+using Basket.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Basket.API.Infrastructure
+{
+    public class BasketContext : DbContext, IBasketContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Checkout> Checkouts { get; set; }
+        public DbSet<Models.Basket> Baskets { get; set; }
+
+        public BasketContext(DbContextOptions<BasketContext> options) : base(options) {  }
+    }
+}
