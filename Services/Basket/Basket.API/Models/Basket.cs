4121d31d834ecc54e836b208ded10c4443d5744c
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2021-03-14 23:46:16 +0300

    feat: add models

---
+++ Services/Basket/Basket.API/Models/Basket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Models
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+
+        public List<Item> Items { get; set; }
+
+        public Basket(int customerId)
+        {
+            Id = customerId;
+        }
+    }
+}
---
+++ Services/Basket/Basket.API/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿namespace Basket.API.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public double ProductPrice { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string PictureUrl { get; set; }
+    }
+}
