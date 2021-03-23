ecb649e3762b4ecedccfd917f3df79e4f5b21c8d
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2021-03-23 22:55:36 +0300

    feat: add jaeger service

--- ApiGateway/Startup.cs
+++ ApiGateway/Startup.cs
@@ -1,7 +1,6 @@
 using ApiGateway.Common.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,7 +29,9 @@ namespace ApiGateway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddJwtService(Configuration);
-            services.AddOcelot();
+            services.AddOcelot(Configuration);
+            services.AddOpenTracing();
+            services.AddJaegerService(Configuration, Environment);
 
             services.AddCors();
         }
