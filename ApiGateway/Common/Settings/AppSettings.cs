378682bee9227b76786a5912459913ce7d1d0237
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2021-03-23 21:04:56 +0300

    feat: add jwt configuration

---
+++ ApiGateway/Common/Settings/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiGateway.Common.Settings
+{
+    public class AppSettings
+    {
+        /// <summary>
+        /// JWT Secret
+        /// </summary>
+        public string Secret { get; set; }
+    }
+}
--- ApiGateway/Startup.cs
+++ ApiGateway/Startup.cs
@@ -1,24 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ApiGateway.Common.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
 
 namespace ApiGateway
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public IHostEnvironment Environment { get; }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddJwtService(Configuration);
+            services.AddOcelot();
+
+            services.AddCors();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
