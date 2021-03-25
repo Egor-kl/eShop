eb1263dfa000b66b9ec9f84cafe291c98d451848
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2021-03-25 22:35:55 +0300

    feat: add first abstract event

---
+++ EventBus/Common/IEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EventBus.Common
+{
+    /// <summary>
+    ///Message broker events.
+    /// </summary>
+    public interface IEvent
+    {
+        /// <summary>
+        /// Event Id.
+        /// </summary>
+        public Guid EventId { get; set; }
+    }
+}
---
+++ EventBus/Events/IProfileCreate.cs
@@ -0,0 +1,17 @@
+﻿using EventBus.Common;
+
+namespace EventBus.Events
+{
+    public interface IProfileCreate : IEvent
+    {
+        /// <summary>
+        /// User id. Identity
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Profile id
+        /// </summary>
+        public int ProfileId { get; set; }
+    }
+}
