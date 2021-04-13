a8d5c7a0939606630d4209254ab2675245b14766
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2021-04-13 22:43:38 +0300

    feat: add configurations for event and jaeger

---
+++ Services/Email/Email/Common/Extensions/EventBusConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Email.Common.Settings;
+using GreenPipes;
+using MassTransit;
+using MassTransit.OpenTracing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Email.Common.Extensions
+{
+    public static class EventBusConfiguration
+    {
+        public static IServiceCollection AddEventBusService(this IServiceCollection services,
+                                                            IConfiguration configuration,
+                                                            IHostEnvironment environment)
+        {
+            var eventBusSettingsSection = configuration.GetSection("EventBusSettings");
+            var eventBusSettings = eventBusSettingsSection.Get<EventBusSettings>();
+
+            services.AddMassTransit(x =>
+            {
+                x.AddBus(context => Bus.Factory.CreateUsingRabbitMq(cfg =>
+                {
+                    cfg.UseHealthCheck(context);
+
+                    var hostName = eventBusSettings.DockerHostName;
+                    cfg.Host(hostName, eventBusSettings.VirtualHostName, host =>
+                    {
+                        host.Username(eventBusSettings.UserName);
+                        host.Password(eventBusSettings.Password);
+                    });
+
+                    cfg.PropagateOpenTracingContext();
+
+                    cfg.ReceiveEndpoint("user-events", ep =>
+                    {
+                        ep.PrefetchCount = 16;
+                        ep.UseMessageRetry(r => r.Interval(2, 100));
+
+                    });
+                }));
+            });
+
+            services.AddMassTransitHostedService();
+
+            services.AddSingleton<IPublishEndpoint>(provider => provider.GetRequiredService<IBusControl>());
+            services.AddSingleton<ISendEndpointProvider>(provider => provider.GetRequiredService<IBusControl>());
+            services.AddSingleton<IBus>(provider => provider.GetRequiredService<IBusControl>());
+
+            return services;
+        }
+    }
+}
