d0f8f71b58b5608d26bf20a5743508f4c76667bd
Egor-kl <57603709+Egor-kl@users.noreply.github.com>
2021-06-13 11:46:40 +0300

    feat: add basket service

---
+++ Services/Basket/Basket.API/DTO/ChrckoutDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Basket.API.DTO
+{
+    public class ChrckoutDTO
+    {
+        public int Id { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string CardNumber { get; set; }
+
+        public string CardHolderName { get; set; }
+
+        /// <summary>
+        ///     Срок действия
+        /// </summary>
+        public DateTime CardExpiration { get; set; }
+
+        /// <summary>
+        ///     CVV
+        /// </summary>
+        public string CardSecurityNumber { get; set; }
+        public BasketDTO Basket { get; set; }
+    }
+}
